7e77d35a6a32badc022b6321aea7e35070e5c6c3
lance-malaga <lmalaga@my.bcit.ca>
2023-01-19 22:34:14 -0800

    update Home page and Card component

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,22 +1,24 @@
+import styles from './styles.module.css'
+
 export default function Card({
     degree = "none",
     color = "purple",
     font = "14px",
-    image = "icon/personTwo.png"
+    image = "icon/personThree.png"
 }) {
     return (
-        <div style={{backgroundColor: color, fontSize: font}}>
+        <div className={styles.card_container} style={{fontSize: font,}}>
             { color === "purple" ? (
                 <img src="icon/person.png" width="50px"/>
             ) :
             color ==="blue" ? (
-                <img src="icon/personThree.png" width="50px"/>
+                <img src="icon/personTwo.png" width="50px"/>
             ) : (
                 <img src={image} width="50px"/>
             )}
-            <li>
+            <p>
                 {degree}
-            </li>
+            </p>
         </div>
     )
 }
